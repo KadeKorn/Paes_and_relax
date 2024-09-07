1bdeb3a2ac7cb6a690cd9c9f8d920802fb43ab44
Kade <kadekorn@gmail.com>
2024-09-07 15:02:16 -0500

    updated the footer and app.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ChakraProvider, Box, VStack, Heading } from '@chakra-ui/react';
+import { ChakraProvider, Box, VStack } from '@chakra-ui/react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './components/HomePage';
@@ -7,11 +7,12 @@ import AboutUs from './components/AboutUs';
 import ServicesPage from './components/ServicesPage';
 import BookingSection from './components/BookingSection';
 import TestimonialsPage from './components/TestimonialsPage';
+import './App.css'; // Ensure this is imported
 
 function App() {
   return (
     <ChakraProvider>
-      <Box>
+      <Box minHeight="100vh"> {/* Ensure this Box takes up the full viewport height */}
         <Header />
         <VStack spacing={8} align="stretch" p={5}>
           <HomePage />
