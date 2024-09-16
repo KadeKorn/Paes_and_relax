bfc840f312f5323b6b3db85a4b74434e3914271e
Kade <kadekorn@gmail.com>
2024-09-16 14:58:59 -0700

    Removing code related to the snap scroll

--- src/App.js
+++ src/App.js
@@ -7,27 +7,27 @@ import AboutUs from './components/AboutUs';
 import ServicesPage from './components/ServicesPage';
 import BookingSection from './components/BookingSection';
 import TestimonialsPage from './components/TestimonialsPage';
-import './App.css'; // Ensure this is imported
+import './App.css';
 
 function App() {
   return (
     <ChakraProvider>
-      <Box minHeight="100vh"> {/* Ensure this Box takes up the full viewport height */}
+      <Box minHeight="100vh"> 
         <Header />
         <VStack spacing={0} align="stretch">
-          <Box className="section" id="home">
+          <Box id="home">
             <HomePage />
           </Box>
-          <Box className="section" id="about">
+          <Box id="about">
             <AboutUs />
           </Box>
-          <Box className="section" id="services">
+          <Box id="services">
             <ServicesPage />
           </Box>
-          <Box className="section" id="booking">
+          <Box id="booking">
             <BookingSection />
           </Box>
-          <Box className="section" id="testimonials">
+          <Box id="testimonials">
             <TestimonialsPage />
           </Box>
         </VStack>
